1b8ff4b6d6aafb4a1db490c94e386bfb76b5e63c
Linerre <leon.errelin@outlook.com>
2025-03-05 22:43:53 +1100

    Add one more solution to leetcode 34

--- clojure/binary_search/first_last_pos.clj
+++ clojure/binary_search/first_last_pos.clj
@@ -2,6 +2,7 @@
 
 ;;; Leetcode 34: https://leetcode.cn/problems/find-first-and-last-position-of-element-in-sorted-array/description/
 
+;; [left, right]
 (defn lower-bound [nums target]
   (loop [left 0
          right (dec (count nums))]
@@ -13,13 +14,39 @@
           (recur left (dec mid)))))))
 
 
-(defn solution [nums target]
+(defn solution1 [nums target]
   (let [start (lower-bound nums target)]
     (if (or (= start (count nums))
             (not= target (get nums start)))
       ;; element does not exist
       [-1, -1]
-      (let [end (lower-bound nums (inc target))]
-        [start (dec end)]))))
+      (let [end (dec (lower-bound nums (inc target)))]
+        [start end]))))
 
-(assert (= [3,4] (solution [5,7,7,8,8,10] 8)))
+(assert (= [3,4] (solution1 [5,7,7,8,8,10] 8)))
+(assert (= [-1,-1] (solution1 [] 8)))
+
+;; [left, right)
+(defn lower-bound-2 [nums target]
+  (loop [left 0
+         right (count nums)]
+    (if (<= right left)
+      left
+      (let [mid (quot (+ left right) 2)]
+        (if (<= target (nth nums mid))
+          (recur left mid)
+          (recur (inc mid) right))))))
+
+;; TODO: (left, right)
+
+(defn solution2 [nums target]
+  (let [start (lower-bound-2 nums target)]
+    (if (or (= start (count nums))
+            (not= target (get nums start)))
+      ;; element does not exist
+      [-1, -1]
+      (let [end (dec (lower-bound-2 nums (inc target)))]
+        [start end]))))
+
+(assert (= [3,4] (solution2 [5,7,7,8,8,10] 8)))
+(assert (= [-1,-1] (solution2 [] 8)))
