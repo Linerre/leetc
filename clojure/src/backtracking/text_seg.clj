237e8bbccfdb2a223762c0600195f727dc8ded03
Linerre <leon.errelin@outlook.com>
2025-04-01 17:56:20 +1100

    Add Index formulation of text segmentation

--- clojure/src/backtracking/text_seg.clj
+++ clojure/src/backtracking/text_seg.clj
@@ -1,10 +1,11 @@
 (ns backtracking.text-seg
   (:require clojure.string as cstr))
 
-;;;
-;;;
-;;;
+;;; Implement algorithm in chapter 2.5 of Algorithms by Jeff Erickson
+;;; see: https://jeffe.cs.illinois.edu/teaching/algorithms/
 
+;;; In the worst case, `splittable` explores each of 2^(n-1) possiblities of
+;;; a string of length n. (pp. 85)
 ;;; A set of words considered acceptable and a function that checks if an
 ;;; input string is indeed an acceptable word.  Note the definition of `word`
 ;;; can vary a lot. In this case, a `word` is just an English word, but it
@@ -22,7 +23,7 @@
 
 (defn word? [w] (contains? words w))
 
-;; My deadloop version
+;; My buggy version (deadloop!)
 #_(defn splittable
   "Return true if the argument string can be split into separate words,
   otherwise false. An empty string is split into zero words and thus splittable."
@@ -38,6 +39,7 @@
 
 ;; Claude suggested this working version
 (defn splittable [s]
+  #_(println "s: " s)
   (if (empty? s)
     true
     (loop [i 1]
@@ -45,3 +47,28 @@
         (> i (count s)) false
         (word? (subs s 0 i)) (and (splittable (subs s i)) true)
         :else (recur (inc i))))))
+
+;;; Index formualtion
+;;; or purposes of designing the algorithm, it’s incredibly useful
+;;; to treat the original input array as a global variable,
+;;; a global string to be used together with the gloabl set of words
+(def input-string "HEARTHANDSATURNSPIN")
+
+(defn is-word?
+  "For any indices i and j , let IsWord(i, j) = T iff
+  the substring A[i .. j] is a word"
+  [i, j]
+  (contains? words (subs input-string i j)))
+
+(defn splittable-idx
+  "For any index i , let Splittable(i) = T iff
+  the sux A[i .. n] can be split into words."
+  [i]
+  #_(println "s: " s)
+  (if (<= (count input-string) i)       ; i starts at 0
+    true
+    (loop [j (inc i)]                   ; j starts at 1
+      (cond
+        (> j (count input-string)) false
+        (is-word? i j) (and (splittable-idx j) true)
+        :else (recur (inc j))))))
