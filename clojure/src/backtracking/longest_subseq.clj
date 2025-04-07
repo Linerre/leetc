b012773099f85f0621402f2ab52d63e3bc87042d
Linerre <leon.errelin@outlook.com>
2025-04-07 13:08:48 +1000

    Fix longest increasing subsequence (Claude version)

--- clojure/src/backtracking/longest_subseq.clj
+++ clojure/src/backtracking/longest_subseq.clj
@@ -6,27 +6,28 @@
 ;;; The below version is not tail optiomized recursion
 (defn lisbigger
   "Find the longest increasing subsequence in the ginven vector `nums`.
-  `prev` is the starting point and must be the number at index 0.
+  `prev` must be smaller than any numbers in `nums`. ##-Inf might be a good choice.
   Return the length of the such a sequence."
   [prev nums]
   (let [next (first nums)
         rm (rest nums)]
     (cond
       (empty? nums) 0
-      (nil? next) 1
-      (empty? rm) (if (< prev next) 2 1)
+      (nil? next) 0
+      (empty? rm) (if (< prev next) 1 0)
       (<= next prev) (lisbigger prev rm)
       :else
       (let [skip (lisbigger prev rm)
             take (lisbigger next rm)]
         (max skip (inc take))))))
 
-(assert (= 2 (lisbigger 4 [4 3 1 9])))
-(assert (= 4 (lisbigger 3 [3, 1, 5, 2, 7, 4, 8])))
-(assert (= 5 (lisbigger 1 [1 2 3 4 5])))
+(assert (= 2 (lisbigger 0 [4 3 1 9])))
+(assert (= 4 (lisbigger 0 [3, 1, 5, 2, 7, 4, 8])))
+(assert (= 4 (lisbigger ##-Inf [3, 1, 5, 2, 7, 4, 8])))
+(assert (= 5 (lisbigger 0 [1 2 3 4 5])))
 
-;;; Suggested by Claude which is buggy
-#_(defn lisbigger
+;;; Suggested by Claude
+(defn lisbigger2
   "Find the longest increasing subsequence where all elements are larger than A[i]."
   [nums i j]
   (cond
@@ -34,17 +35,28 @@
     0
 
     (>= (get nums i) (get nums j))
-    (lisbigger nums i (inc j))
+    (lisbigger2 nums i (inc j))
 
     :else
-    (let [skip (lisbigger nums i (inc j))
-          take (+ 1 (lisbigger nums j (inc j)))]
+    (let [skip (lisbigger2 nums i (inc j))
+          take (+ 1 (lisbigger2 nums j (inc j)))]
       (max skip take))))
 
-;; To find LIS starting from index 0
+;; To find LIS starting from index 0, the number indexed at 0
+;; must be the smallest in the array. Simplest approach is to use neg infinity
+(defn lis
+  "Find the longest increasing subsequence in the given vector nums."
+  [nums]
+  (if (empty? nums)
+    0
+    ;; Create a new array with a sentinel value at the beginning
+    (let [sentinel-value ##-Inf  ;; Clojure's representation of negative infinity
+          nums-with-sentinel (into [sentinel-value] nums)]
+      (lisbigger2 nums-with-sentinel 0 1))))
+
 #_(defn lis [nums]
   (if (empty? nums)
     0
-    (lisbigger nums 0 1)))
+    (lisbigger2 nums 0 1)))
 
-#_(comment (lis [3 1 5 2 7 4 8]))
+(assert (= 4 (lis [3 1 5 2 7 4 8])))
