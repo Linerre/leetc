043f03c73332ea6479a3d36ac31cb3d2014d95e4
Linerre <leon.errelin@outlook.com>
2025-09-18 16:17:00 +1000

    Fix leetcode92

--- clojure/src/linkedlist/single.clj
+++ clojure/src/linkedlist/single.clj
@@ -8,7 +8,10 @@
 
 (deftype ListNode [value next]
   ISinglyList
-  (set-next [this next-node] (ListNode. (.value this) next-node))
+  (set-next [this next-node]
+    (if (identical? (.next this) next-node)
+      this
+      (ListNode. (.value this) next-node)))
   (count-nodes [this] (loop [cnt 1
                              cur this]
                         (if (nil? (.next cur))
@@ -27,21 +30,8 @@
   [value]
   (ListNode. value nil))
 
-(defn insert-node
-  "Insert a new node with the given value at the node.
-  This is equivalent to making the new node's next point to node.
-  If the given node is not head, the inserted new node becomes the new head."
-  [current new-node]
-  (ListNode. (.value current) new-node))
-
-(defn list-len
-  "Always assume the list contains at least one node."
-  [head]
-  (loop [cnt 1
-         cur head]
-    (if (nil? (.next cur))
-      cnt
-      (recur (inc cnt) (.next cur)))))
+(defn prepend-node [current value]
+  (ListNode. value current))
 
 (defn print-list
   [head]
@@ -59,31 +49,28 @@
          cur head]
     (if (nil? cur)
       prev
-      (recur (insert-node cur prev) (.next cur)))))
+      (recur (prepend-node (.value cur) prev) (.next cur)))))
+
+(defn find-prev
+  "Position is 1-indexed and is guaranteed to be <= list length.
+  This is equivalent to return previous node of the given positoin.
+  If the given position is 1 (head), return the dummy node."
+  [dummy position]
+  (loop [curn dummy
+         curp 0]
+    (if (= curp (dec position))
+      curn
+      (recur (.next curn) (inc curp)))))
 
 (comment
   (-> (ListNode. 1 (ListNode. 2 (ListNode. 3 (ListNode. 4 (ListNode. 5 nil)))))
-      reverse-list
-      print-list))
-
-(defn find-prev
-  "Position is 1-indexed and is guaranteed to be <= list length."
-  [head position]
-  (loop [curn head
-         curp (dec position)]
-    (if (= 0 curp)
-      curn
-      (recur (.next curn) (dec curp)))))
-
-(comment
-  (-> (ListNode. 1 nil)
-      (insert-node (ListNode. 2 (ListNode. 3 (ListNode. 4 (ListNode. 5 nil)))))
-      (find-prev 4)
+      (prepend-node "dummy")
+      (find-prev 1)
       (.value)))
 
 (defn find-next
-  "position is guaranteed to be <= list length.
-  If position points to the last node, return last node intead."
+  "Position is 1-indexed is guaranteed to be <= list length.
+  No need for dummy in this search"
   [head position]
   (loop [curn head
          curp 1]
@@ -91,32 +78,73 @@
       (.next curn)
       (recur (.next curn) (inc curp)))))
 
+(defn find-node
+  [head position]
+  (loop [curn head
+         curp 1]
+    (if (= curp position)
+      curn
+      (recur (.next curn) (inc curp)))))
+
 (comment
-  (-> (ListNode. 1 nil)
-      (insert-node (ListNode. 2 (ListNode. 3 (ListNode. 4 (ListNode. 5 nil)))))
-      (find-next 5)))
+  (-> (ListNode. 1 (ListNode. 2 (ListNode. 3 (ListNode. 4 (ListNode. 5 nil)))))
+      (find-node 4)
+      (.value)))
 
 ;; Leetcode 92
 ;; 1 -> 2 -> 3 -> 4 -> 5
 ;; 1 -> 5 -> 4 -> 3 -> 2 (left = 2, right = 5)
 ;; 1 -> 4 -> 3 -> 2 -> 5 (left = 2, right = 4)
-(defn reverse-list-partial
+(defn reverse-between
   "Reverse the part (1 <= left <= right) of the given list starting with head."
   [head left right]
-  (let [dummy    (insert-node (ListNode. -1 nil) head)
+  (let [dummy    (prepend-node head "dummy")
         nl       (find-prev dummy left)
         ;; FIXME: better to not run this search
+        ;; get the rest list immediately after right
         nr       (find-next head right)]
-    (loop [prev nr
+    (loop [prev nr                 ; start prepending onto to the rest
            curn (.next nl)
            cnt  0]
       (if (= cnt (+ 1 (- right left)))
-        (cond-> nl
-          (< 1 left)
-          (insert-node prev))
-        (recur (insert-node curn prev) (.next curn) (inc cnt))))))
+        (loop [oprev nl
+               ret prev
+               pos (dec left)]
+          (if (= pos 0)
+            ret
+            (recur (find-prev dummy pos)
+                   (prepend-node ret (.value oprev))
+                   (dec pos))))
+        (recur (prepend-node prev (.value curn)) (.next curn) (inc cnt))))))
 
 (comment
   (-> (ListNode. 1 (ListNode. 2 (ListNode. 3 (ListNode. 4 (ListNode. 5 nil)))))
-      (reverse-list-partial 2 4)
+      (reverse-between 1 2)
+      (print-list)))
+
+;; Leetcode 25
+#_(defn reverse-k-nodes
+    "Split the linked list into len / k groups and reverse each group."
+    [head k]
+    (let [n (count-nodes head)
+          dummy (insert-node (ListNode. -1 nil) head)]
+      (loop [m n
+             step k
+             prev nil
+             curn (.next dummy)]
+        (if (< m k)
+          prev
+          (recur (- m k)
+                 (+ k step)
+                 (reverse-list-partial curn (dec step) k)
+                 (-> curn .next .next))))))
+
+#_(comment
+  (-> (ListNode. 1 (ListNode. 2 (ListNode. 3 (ListNode. 4 (ListNode. 5 nil)))))
+      (reverse-list-partial 1 2)
+      (print-list)))
+
+#_(comment
+  (-> (ListNode. 1 (ListNode. 2 (ListNode. 3 (ListNode. 4 (ListNode. 5 nil)))))
+      (reverse-k-nodes 2)
       (print-list)))
