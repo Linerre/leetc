8ca0322f9fb175c9c35784cd08751fb24c70e694
Linerre <leon.errelin@outlook.com>
2026-07-19 21:10:05 +0800

    Add quickSort v1 in TypeScript

---
+++ typescript/src/lect023/main_test.ts
@@ -0,0 +1,9 @@
+import { assertEquals } from 'jsr:@std/assert';
+import { quickSort } from './quick-sort.ts';
+
+Deno.test('Test quickSort case-1', () => {
+    const nums = [1,3,5,2,4,6];
+    // console.log('x:', nums[0 + Math.floor((Math.random() * (5 - 0 + 1)))])
+    quickSort(nums, 0, nums.length-1);
+    assertEquals(nums, [1,2,3,4,5,6]);
+})
---
+++ typescript/src/lect023/quick-sort.ts
@@ -0,0 +1,48 @@
+/**
+ * (For a quicksort pivot between `left` and `right` inclusive, use:
+ * Math.floor(left + Math.random() * (right - left + 1)))
+ */
+export function quickSort(nums: number[], l: number, r: number): void {
+    // invalid range: either 1 number or no number
+    if (l >= r) return;
+
+    // Randomly selected a number from the given range [l...r]
+    // r - l + 1 = length of the range,
+    // random fn * range to get a random number of [0...len-1]
+    const x = nums[Math.floor(l + Math.random() * (r - l + 1))];
+    const mid = partition1(nums, l, r, x);
+    // mid = a - 1 will remain unchanged so move right bound left by 1 number
+    quickSort(nums, l, mid - 1);
+    quickSort(nums, mid + 1, r);
+}
+
+// Modify the array in place so that in range [l...r]:
+// 1. [l...a-1] contains numbers <= x (randomly selected)
+// 2. [a...r] contains numbers > x (randomly selected)
+// return the mid position for recursion
+function partition1(nums: number[], l: number, r: number, x: number): number {
+    let a = l;                  // a marks the boundary where nums[a] > x
+    let xi = 0;                 // also remember any index where nums[xi] = x
+    for (let i = l; i <= r; i++) {
+        if (nums[i] <= x) {
+            swap(nums, a, i);
+            if (nums[a] === x)
+                xi = a;
+
+            // extend the range by moving a forward
+            a++;
+        }
+        // if current num > x, move i forward only
+    }
+
+    // put x at the last in the range where all nums <= x
+    swap(nums, xi, a - 1);
+    return a - 1;
+}
+
+function swap(nums: number[], i: number, j: number): void {
+    // if i === j, can return early
+    const tmp = nums[i];
+    nums[i] = nums[j];
+    nums[j] = tmp;
+}
