d9a234a28fd81a65a5e15560b3699c414ba423dc
Linerre <leon.errelin@outlook.com>
2026-08-02 15:48:55 +0800

    Add heap implementation for later use

--- typescript/src/lect027/mergeKLists.ts
+++ typescript/src/lect027/mergeKLists.ts
@@ -33,3 +33,52 @@ export function mergeKLists(lists: Array<ListNode | null>): ListNode | null {
     }
     return h;
 };
+
+/**
+ *************************************************************
+ * Implement a heap (small-root) and use it as priority queue
+ *************************************************************
+ */
+
+/**
+ * Re-arrange an array into a heap where any substree has smallest number at top
+ * @param: nums the array that represents a heap
+ * @param: i the index at which the number to be inserted
+ */
+export function heapInsert(nums: number[], i: number): void {
+    while (nums[i] < nums[Math.floor((i-1)/2)]) {
+        swap(nums, i, Math.floor((i-1)/2));
+        i = Math.floor((i-1)/2);
+    }
+}
+
+/**
+ * Push smaller number up to maintain the large-root heap structure
+ * @param: nums the array the represents a heap
+ * @param: i index of current number
+ * @param: size the array/heap size
+ */
+export function heapify(nums: number[], i: number, size: number): void {
+    let l = i * 2 + 1;          // left child
+    while (l < size) {
+        // Has left child and right child
+        // Decide which one is smaller
+        let best = l + 1 < size && nums[l + 1] < nums[l] ? l + 1 : l;
+        // Decide if best child is larger than current number
+        best = nums[best] < nums[i] ? best : i;
+        if (best === i) break;
+        // push up smaller one
+        swap(nums, best, i);
+        // Update current idx to best (one of left or right child) and continue
+        i = best;
+        l = i * 2 + 1;
+    }
+}
+
+// Assume both i and j <= nums.length - 1
+function swap(nums: number[], i: number, j: number): void {
+    if (i === j || nums[i] === nums[j]) return;
+    const temp = nums[i];
+    nums[i] = nums[j];
+    nums[j] = temp;
+}
