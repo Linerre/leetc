a2b03234fc3bda60c955c045223f6e35db0d85fa
Linerre <leon.errelin@outlook.com>
2026-08-10 23:42:11 +0800

    Minor bugfix

--- typescript/src/lect028/main.ts
+++ typescript/src/lect028/main.ts
@@ -2,8 +2,33 @@
 
 const BASE = 10;
 const MAXN = 5000;
-const help = Array(MAXN);
-const cnts = Array(BASE);
+const help: number[] = Array(MAXN);
+const cnts: number[] = Array(BASE);
+
+export function sortArray(nums: number[]): number[] {
+    if (nums.length > 1) {
+		const n = nums.length;
+        // find the min in array
+		let min = nums[0];
+		for (let i = 1; i < n; i++) {
+			min = Math.min(min, nums[i]);
+		}
+		let max = 0;
+		for (let i = 0; i < n; i++) {
+            // make each number in nums non-negative
+			nums[i] -= min;
+			// find the max
+			max = Math.max(max, nums[i]);
+		}
+		// max decides the digits within BASE
+		radixSort(nums, n, digits(max));
+		// restore the nums
+		for (let i = 0; i < n; i++) {
+			nums[i] += min;
+		}
+	}
+	return nums;
+}
 
 // Return number of digits of the given number according to BASE
 function digits(n: number): number {
@@ -20,7 +45,7 @@ function digits(n: number): number {
  * @param n array length
  * @param digits of the max number in array in BASE
  */
-function radixSort(num: number[], n: number, digits: number): void {
+function radixSort(nums: number[], n: number, digits: number): void {
     // Loop through each digit in the number
     for (let offset = 1;  digits > 0; offset *= BASE, digits--) {
         cnts.fill(0);
@@ -34,19 +59,23 @@ function radixSort(num: number[], n: number, digits: number): void {
         // Thus at offset 10, digit 3 appears once
         // Thus at offset 10, digit 2 appears once
         for (let i = 0; i < n; i++) {
-            const d = Math.floor(nums[i] / offest) % BASE;
-            cnt[d] += 1;
+            const d = Math.floor(nums[i] / offset) % BASE;
+            cnts[d] += 1;
         }
         // Add up prefixes. For example,
         // if at digit 1, 0 appears 10 times and at digit 2, 3 appear twice,
         // then increase the time of appearance for 3 to 13
         // That is to say, at any digit, the cnt[i] means the total counts of numbers <= cnts[i]
-        for (let i = 0; i < BASE; i++) {
+        for (let i = 1; i < BASE; i++) {
             cnts[i] = cnts[i] + cnts[i-1];
         }
+        // console.debug('cnts', cnts);
         // Iterate in reverse order to put numbers in nums to help
         for (let i = n - 1; i >=0; i--) {
-            const d = Math.floor(nums[i] / offest) % BASE;
+            // Put ith number in nums to cnts[d] - 1 in help;
+            // Then decrease cnts[d]
+            // This is equivalent to decreasing cnts[d] and then putting ith number in nums to help
+            const d = Math.floor(nums[i] / offset) % BASE;
             cnts[d] -= 1;
             help[cnts[d]] = nums[i];
         }
