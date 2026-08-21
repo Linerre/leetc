476d6f5b8d93d5c8a145572f047844c9f629a3d4
Linerre <leon.errelin@outlook.com>
2026-08-21 15:19:16 +0800

    Add one more test for `getIntersectionNode`

--- typescript/src/lect034/main_test.ts
+++ typescript/src/lect034/main_test.ts
@@ -1,4 +1,4 @@
-import { assert, assertFalse, assertExists, assertEquals } from 'jsr:@std/assert';
+import { assert, assertFalse, assertExists, assertEquals, assertNotEquals  } from 'jsr:@std/assert';
 import {
     ListNode,
     getIntersectionNode,
@@ -37,7 +37,6 @@ function makeListFromArray(nums: number[]): ListNode | null {
     return head;
 }
 
-
 function intersect(
     headA: ListNode | null,
     headB: ListNode | null,
@@ -49,6 +48,7 @@ function intersect(
     let a = headA;
     let b = headB;
     let skip = 0;
+    // At most a moves to the last node
     while (skip < skipA && a.next) {
         a = a.next;
         skip++;
@@ -60,12 +60,13 @@ function intersect(
     }
 
     // no intersection
-    if (b.next === null) {
-        console.log('here');
-        return false;
-    }
+    if (b.next === null) return false;
 
-    // intersect at a
+    // intersect at the node with the same value. A: 1->2->5 A: 4->7->5
+    // if skipA === skipB === 3, then the two lists will still
+    // intersect even your intention is different. To pretend they are
+    // not intersected, skipA must not equal skipB. This has nothing to
+    // do with the solution, just to make the simulation correct.
     if (a.val === b.next.val) {
         b.next = a;
         return true;
@@ -105,4 +106,40 @@ Deno.test('Test getIntersectionNode 1', () => {
     assertExists(intersection);
     assertEquals(intersection.val, stopA);
     assertEquals(intersection.val, stopB);
+})
+
+Deno.test('Test getIntersectionNode 2', () => {
+    const listA = [1,9,1,2,4], listB = [3,2,4];
+    const skipA = 3, skipB = 1;
+    const hA = makeListFromArray(listA);
+    const hB = makeListFromArray(listB);
+    assertExists(hA);
+    assertExists(hB);
+    const stopA = skipToNode(hA, skipA);
+    const stopB = skipToNode(hB, skipB);
+    assertEquals(stopA, stopB);
+    // intersect
+    assert(intersect(hA,hB,skipA,skipB));
+    const intersection = getIntersectionNode(hA, hB);
+    // console.log(intersection);
+    assertExists(intersection);
+    assertEquals(intersection.val, stopA);
+    assertEquals(intersection.val, stopB);
+})
+
+Deno.test('Test getIntersectionNode 3', () => {
+    const listA = [2,6,4], listB = [1,5];
+    const skipA = 3, skipB = 2;
+    const hA = makeListFromArray(listA);
+    const hB = makeListFromArray(listB);
+    assertExists(hA);
+    assertExists(hB);
+    const stopA = skipToNode(hA, skipA);
+    const stopB = skipToNode(hB, skipB);
+    assertNotEquals(stopA, stopB);
+    // intersect
+    assertFalse(intersect(hA,hB,skipA,skipB));
+    const intersection = getIntersectionNode(hA, hB);
+    // console.log(intersection);
+    assert(intersection === null, 'Expected intersection to be null but got non-null value');
 })
