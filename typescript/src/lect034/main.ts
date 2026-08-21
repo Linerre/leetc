d774199ec730e05fba8734a033baee1bcaa03a92
Linerre <leon.errelin@outlook.com>
2026-08-21 14:47:20 +0800

    Remove unnecessary ! assertions

--- typescript/src/lect034/main.ts
+++ typescript/src/lect034/main.ts
@@ -1,6 +1,6 @@
 
 // Simple 160: https://leetcode.cn/problems/intersection-of-two-linked-lists/
-class ListNode {
+export class ListNode {
     val: number
     next: ListNode | null
     constructor(val?: number, next?: ListNode | null) {
@@ -9,23 +9,22 @@ class ListNode {
     }
 }
 
-
-function getIntersectionNode(headA: ListNode | null, headB: ListNode | null): ListNode | null {
+export function getIntersectionNode(headA: ListNode | null, headB: ListNode | null): ListNode | null {
     if (headA == null || headB == null) return null;
 
     // Find the length diff of two lists
-    let a: ListNode | null = headA;
-    let b: ListNode | null = headB;
+    let a = headA;
+    let b = headB;
     let diff: number = 0;
 
     // At this place, a can't be null but we force compiler to skip null-checking
     // Same goes for b and through to the end
-    while (a!.next !== null) {
+    while (a.next !== null) {
         a = a.next;
         diff++;
     }
 
-    while (b!.next !== null) {
+    while (b.next !== null) {
         b = b.next;
         diff--;
     }
@@ -44,15 +43,15 @@ function getIntersectionNode(headA: ListNode | null, headB: ListNode | null): Li
 
     diff = Math.abs(diff);
     // Move `diff` on the longer list so that we have the same length left as the shorter
-    while (diff !== 0) {
-        a = a!.next;
+    while (diff !== 0 && a.next) {
+        a = a.next;
         diff--;
     }
 
     // Now find where a and b intersects
-    while (a !== b) {
-        a = a!.next;
-        b = b!.next;
+    while (a !== b && a.next && b.next) {
+        a = a.next;
+        b = b.next;
     }
 
     return a;
