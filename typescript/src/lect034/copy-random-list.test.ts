0ca63a9ad44c6ac71b7fb19f402e3602b83e9309
Linerre <leon.errelin@outlook.com>
2026-08-23 17:25:32 +0800

    Add tests for copyRandomList

--- typescript/src/lect034/copy-random-list.test.ts
+++ typescript/src/lect034/copy-random-list.test.ts
@@ -13,6 +13,32 @@ Deno.test({
     fn: () => {
         const pairs: NodePairs = [[7,null],[13,0],[11,4],[10,2],[1,0]];
         const head = makeListWithRandomFromArray(pairs);
+        const copyHead = copyRandomList(head);
         assertEquals(printListWithRandom(head), pairs);
+        assertEquals(printListWithRandom(copyHead), pairs);
+    }
+});
+
+Deno.test({
+    name: 'Test copyRandomList 2',
+    timeout: 5000,
+    fn: () => {
+        const pairs: NodePairs = [[1,1],[2,1]];
+        const head = makeListWithRandomFromArray(pairs);
+        const copyHead = copyRandomList(head);
+        assertEquals(printListWithRandom(head), pairs);
+        assertEquals(printListWithRandom(copyHead), pairs);
+    }
+});
+
+Deno.test({
+    name: 'Test copyRandomList 3',
+    timeout: 5000,
+    fn: () => {
+        const pairs: NodePairs = [[3,null],[3,0],[3,null]];
+        const head = makeListWithRandomFromArray(pairs);
+        const copyHead = copyRandomList(head);
+        assertEquals(printListWithRandom(head), pairs);
+        assertEquals(printListWithRandom(copyHead), pairs);
     }
 });
