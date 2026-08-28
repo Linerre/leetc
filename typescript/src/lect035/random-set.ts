afd7107971d8a5e77cf8149a401c6c95c8c3708d
Linerre <leon.errelin@outlook.com>
2026-08-28 16:35:06 +0800

    Add Leetcode 380: RandomizeSet in TypeScript

---
+++ typescript/src/lect035/random-set.ts
@@ -0,0 +1,32 @@
+export class RandomizedSet {
+  private map: Map<number, number>;
+  private arr: number[];
+
+  constructor() {
+    this.map = new Map<number, number>();
+    this.arr = new Array<number>();
+  }
+
+  insert(val: number): boolean {
+    if (this.map.has(val)) return false;
+    this.map.set(val, this.arr.length);
+    this.arr.push(val);
+    return true;
+  }
+
+  remove(val: number): boolean {
+    const valIndex: number | undefined = this.map.get(val);
+    if (valIndex === undefined) return false;
+    const lastIndex = this.arr.length - 1;
+    const lastVal = this.arr[lastIndex];
+    this.map.set(lastVal, valIndex);
+    this.map.delete(val);
+    this.arr[valIndex] = lastVal;
+    this.arr.pop();
+    return true;
+  }
+
+  getRandom(): number {
+    return this.arr[Math.floor(Math.random() * this.arr.length)];
+  }
+}
