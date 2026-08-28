546ac3fa7bc4fea60e632af13769422c0ec5d127
Linerre <leon.errelin@outlook.com>
2026-08-28 18:56:02 +0800

    Add one more test for RandomizedCollection

--- typescript/src/lect035/randomized-collection.test.ts
+++ typescript/src/lect035/randomized-collection.test.ts
@@ -4,7 +4,7 @@ import { assert } from "@std/assert";
 import { RandomizedCollection } from "./randomized-collection.ts";
 
 Deno.test({
-  name: "Test Randomized collection",
+  name: "Test Randomized collection 1",
   timeout: 1000,
   fn: () => {
     const rc = new RandomizedCollection();
@@ -12,5 +12,19 @@ Deno.test({
     assertFalse(rc.insert(1));
     assert(rc.remove(1));
     assertEquals(rc.getRandom(), 1);
+  },
+});
+
+Deno.test({
+  name: "Test Randomized collection 2",
+  timeout: 1000,
+  fn: () => {
+    const rc = new RandomizedCollection();
+    assert(rc.insert(1));
+    assertFalse(rc.insert(1));
+    assert(rc.insert(2));
+    assert([1, 2].includes(rc.getRandom()));
+    assert(rc.remove(1));
+    assert([1, 2].includes(rc.getRandom()));
   },
 });
