9a4fd721d77f6341005e7efb4edfc855ec55e2fb
Linerre <leon.errelin@outlook.com>
2026-08-28 14:43:41 +0800

    Format the RLU files to have an indentation of 2 spaces

--- typescript/src/lect035/setallmap.test.ts
+++ typescript/src/lect035/setallmap.test.ts
@@ -3,36 +3,36 @@ import { SetAllMap } from './setallmap.ts';
 
 // see: https://www.nowcoder.com/practice/7c4559f138e74ceb9ba57d76fd169967
 Deno.test({
-    name: 'SetAllMap test',
-    timeout: 5000,
-    fn: () => {
-        const map = new SetAllMap();
-        // opt1
-        map.put(1,2);
-        assertEquals(map.get(1), 2);
-        // opt2
-        assertEquals(map.get(1), 2);
-        // opt3
-        assertEquals(map.get(2), -1);
-        // opt4
-        map.setAll(4);
-        // opt5
-        assertEquals(map.get(1), 4);
-        // opt6
-        assertEquals(map.get(2), -1);
-    }
+  name: 'SetAllMap test',
+  timeout: 5000,
+  fn: () => {
+    const map = new SetAllMap();
+    // opt1
+    map.put(1,2);
+    assertEquals(map.get(1), 2);
+    // opt2
+    assertEquals(map.get(1), 2);
+    // opt3
+    assertEquals(map.get(2), -1);
+    // opt4
+    map.setAll(4);
+    // opt5
+    assertEquals(map.get(1), 4);
+    // opt6
+    assertEquals(map.get(2), -1);
+  }
 });
 
 Deno.test({
-    name: 'SetAllMap test',
-    timeout: 5000,
-    fn: () => {
-        const m = new SetAllMap();
-        m.put(5, 17);
-        m.put(6, 100);
-        m.setAll(9);
-        assertEquals(m.get(5), 9);
-        m.put(19, 54);
-        assertEquals(m.get(19), 54);
-    }
+  name: 'SetAllMap test',
+  timeout: 5000,
+  fn: () => {
+    const m = new SetAllMap();
+    m.put(5, 17);
+    m.put(6, 100);
+    m.setAll(9);
+    assertEquals(m.get(5), 9);
+    m.put(19, 54);
+    assertEquals(m.get(19), 54);
+  }
 });
--- typescript/src/lect035/setallmap.ts
+++ typescript/src/lect035/setallmap.ts
@@ -1,32 +1,32 @@
 type TimestampedValue = [number, number];
 
 export class SetAllMap {
-    private map = new Map<number, TimestampedValue>();
-    private setAllValue = 0;
-    private setAllTimestamp = -1;
-    private cnt = 0;
+  private map = new Map<number, TimestampedValue>();
+  private setAllValue = 0;
+  private setAllTimestamp = -1;
+  private cnt = 0;
 
-    put(k: number, v: number): void {
-        if (this.map.has(k)) {
-            const value: TimestampedValue | undefined = this.map.get(k);
-            if (value) {
-                value[0] = v;
-                value[1] = this.cnt++;
-            }
-        } else {
-            this.map.set(k, [v, this.cnt++]);
-        }
+  put(k: number, v: number): void {
+    if (this.map.has(k)) {
+      const value: TimestampedValue | undefined = this.map.get(k);
+      if (value) {
+        value[0] = v;
+        value[1] = this.cnt++;
+      }
+    } else {
+      this.map.set(k, [v, this.cnt++]);
     }
+  }
 
-    setAll(v: number): void {
-        this.setAllValue = v;
-        this.setAllTimestamp = this.cnt++;
-    }
+  setAll(v: number): void {
+    this.setAllValue = v;
+    this.setAllTimestamp = this.cnt++;
+  }
 
-    get(k: number): number {
-        if (!this.map.has(k)) return -1;
-        const value: TimestampedValue | undefined = this.map.get(k);
-        if (value && value[1] > this.setAllTimestamp) return value[0];
-        else return this.setAllValue;
-    }
+  get(k: number): number {
+    if (!this.map.has(k)) return -1;
+    const value: TimestampedValue | undefined = this.map.get(k);
+    if (value && value[1] > this.setAllTimestamp) return value[0];
+    else return this.setAllValue;
+  }
 }
