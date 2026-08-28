02a3226ad3c6799339981e13f734846d91d5b3eb
Linerre <leon.errelin@outlook.com>
2026-08-28 18:41:55 +0800

    Add Leetcode 381: Randomized collection in TypeScript

---
+++ typescript/src/lect035/randomized-collection.ts
@@ -0,0 +1,54 @@
+export class RandomizedCollection {
+  private map: Map<number, Set<number>>;
+  private arr: number[];
+
+  constructor() {
+    this.map = new Map<number, Set<number>>();
+    this.arr = new Array<number>();
+  }
+
+  insert(val: number): boolean {
+    if (this.map.has(val)) return false;
+    let set: Set<number> | undefined = this.map.get(val);
+    if (set === undefined) {
+      set = new Set([this.arr.length]);
+      this.map.set(val, set);
+    } else {
+      set.add(this.arr.length);
+    }
+    this.arr.push(val);
+    return set.size === 1;
+  }
+
+  remove(val: number): boolean {
+    const valSet: Set<number> | undefined = this.map.get(val);
+    if (valSet === undefined) return false;
+
+    const anyValIndex = valSet.values().next().value;
+    if (anyValIndex === undefined) return false;
+
+    const lastIndex = this.arr.length - 1;
+    const lastVal = this.arr[lastIndex];
+    // if val happens to appear at the end of arr, we remove this one
+    if (val === lastVal) {
+      valSet.delete(lastIndex);
+    } else {
+      this.arr[anyValIndex] = this.arr[lastIndex];
+      const lastValSet: Set<number> | undefined = this.map.get(lastVal);
+      if (lastValSet) {
+        lastValSet.add(anyValIndex);
+        lastValSet.delete(lastIndex);
+        valSet.delete(anyValIndex);
+      }
+    }
+    this.arr.pop();
+
+    // If after removal, val has no presence at all, remove it from map
+    if (valSet.size === 0) this.map.delete(val);
+    return true;
+  }
+
+  getRandom(): number {
+    return this.arr[Math.floor(Math.random() * this.arr.length)];
+  }
+}
