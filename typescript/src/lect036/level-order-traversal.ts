c8552197617fe149a0232ac3b257e7fefb50b0c5
Linerre <leon.errelin@outlook.com>
2026-09-01 21:41:58 +0800

    Add one more solution to lc 102 in TypeScript

--- typescript/src/lect036/level-order-traversal.ts
+++ typescript/src/lect036/level-order-traversal.ts
@@ -2,7 +2,7 @@ import { Queue } from '@datastructures-js/queue';
 import { TreeNode } from './util.ts';
 
 // Medium 102: https://leetcode.cn/problems/binary-tree-level-order-traversal/description/
-function levelOrder(root: TreeNode | null): number[][] {
+function levelOrder1(root: TreeNode | null): number[][] {
   const ans: number[][] = [];
   if (root !== null) {
     const queue = new Queue<TreeNode>();
@@ -28,3 +28,40 @@ function levelOrder(root: TreeNode | null): number[][] {
   }
   return ans;
 };
+
+const MAXN = 2001;
+function levelOrder2(root: TreeNode | null): number[][] {
+  const queue: TreeNode[] = Array<TreeNode>(MAXN);
+  let levels: number[][] = [];
+  if (root !== null) {
+    // l marks leftmost node and r - 1 marks the rightmost node
+    // this line is also equivalent to clearing the queue
+    let l = 0;
+    let r = 0;
+    // put root into the queue first
+    queue[r] = root;
+    r++;
+    // repeat until there is nothing in the queue
+    while (l < r) {
+      const size = r - l;
+      let level = new Array<number>();
+      // repeat size times
+      for (let i = 0; i < size; i++) {
+        // 1. take a node from the queue
+        const node = queue[l];
+        l++;
+        level.push(node!.val);
+        if (node && node.left) {
+          queue[r] = node.left;
+          r++;
+        }
+        if (node && node.right) {
+          queue[r] = node.right;
+          r++;
+        }
+      }
+      levels.push(level);
+    }
+  }
+  return levels;
+}
