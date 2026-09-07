4ac43b5b3af00ed39df93963c116e08f16a36b10
Linerre <leon.errelin@outlook.com>
2026-09-07 22:47:28 +0800

    Add minDepth in TypeScript

--- typescript/src/lect036/max-min-depth.ts
+++ typescript/src/lect036/max-min-depth.ts
@@ -1,5 +1,18 @@
 import { TreeNode } from './util.ts';
 
-function maxDepth(root: TreeNode | null): number {
+export function maxDepth(root: TreeNode | null): number {
   return root === null ? 0 : Math.max(maxDepth(root.left), maxDepth(root.right)) + 1;
+};
+
+export function minDepth(root: TreeNode | null): number {
+  if (root === null) return 0;
+  if (root.left === null && root.right === null) return 1;
+
+  let leftDepth = Number.MAX_SAFE_INTEGER;
+  let rightDepth = Number.MAX_SAFE_INTEGER;
+
+  if (root.left) leftDepth = minDepth(root.left);
+  if (root.right) rightDepth = minDepth(root.right);
+
+  return Math.min(leftDepth, rightDepth) + 1;
 };
